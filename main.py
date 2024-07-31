3e3a84adacff0ffe6b187d97998667f845ab27c1
Bd15pw <136353082+Bd15pw@users.noreply.github.com>
2024-07-31 11:16:09 +0000

    cwiczenia

--- main.py
+++ main.py
@@ -1,74 +1,90 @@
 """
-Ćwiczenie 1: Sumowanie liczb parzystych
-Zadanie:
-Napisz program, który sumuje wszystkie liczby parzyste od 1 do 100.
+Zadanie 1: Liczby Parzyste i Nieparzyste
+Napisz program, który poprosi użytkownika o podanie liczby n, a następnie wyświetli wszystkie liczby od 1 do n. Dla każdej liczby, program powinien wskazać, czy jest parzysta, czy nieparzysta.
 
-Objaśnienie:
-Utwórz zmienną, która będzie przechowywać sumę liczb parzystych (np. sum_even).
-Użyj pętli for, aby przejść przez liczby od 1 do 100.
-W każdej iteracji sprawdź, czy liczba jest parzysta (podzielna przez 2 bez reszty).
-Jeśli liczba jest parzysta, dodaj ją do zmiennej sum_even.
-Po zakończeniu pętli wypisz wartość zmiennej sum_even.
+Wskazówki:
+
+Użyj pętli for lub while do iteracji przez liczby od 1 do n.
+Użyj instrukcji if-else do sprawdzenia, czy liczba jest parzysta (liczba % 2 == 0), czy nieparzysta.
+Zadanie 2: Suma Liczb
+Napisz program, który poprosi użytkownika o podanie liczby n, a następnie obliczy sumę wszystkich liczb od 1 do n.
+
+Wskazówki:
+
+Użyj pętli for lub while do iteracji przez liczby od 1 do n.
+Zsumuj te liczby wewnątrz pętli.
+Zadanie 3: Odwracanie Ciągu
+Napisz program, który poprosi użytkownika o podanie ciągu znaków, a następnie wyświetli ten ciąg w odwrotnej kolejności.
+
+Wskazówki:
+
+Użyj pętli for do iteracji przez ciąg znaków od końca do początku.
+Twórz nowy ciąg znaków, dodając kolejne litery w odwrotnej kolejności.
+Zadanie 4: Liczby Pierwsze
+Napisz program, który znajdzie wszystkie liczby pierwsze w zakresie od 1 do n, gdzie n jest podane przez użytkownika.
+
+Wskazówki:
+
+Użyj podwójnej pętli: zewnętrzna pętla for lub while do iteracji przez liczby od 2 do n.
+Wewnętrzna pętla (lub dodatkowa logika) sprawdzi, czy dana liczba ma jakichkolwiek dzielników poza 1 i sobą (czyli sprawdzi, czy jest pierwsza).
+Użyj instrukcji if-else, aby określić i wyświetlić liczby pierwsze.
+Powodzenia w programowaniu! Jeśli napotkasz problemy, zawsze możesz wrócić i zadać pytania lub prosić o wskazówki.
+
+
+
 """
 
-sum_even = 0 
-
-for i in range(101):
-  if i % 2 == 0: 
-    sum_even += i 
-
-print(sum_even)
-
 """
-Ćwiczenie 2: Wypisywanie liczb podzielnych przez 3
-Zadanie:
-Napisz program, który wypisuje wszystkie liczby od 1 do 50, które są podzielne przez 3.
-Objaśnienie:
-Użyj pętli for, aby przejść przez liczby od 1 do 50.
-W każdej iteracji sprawdź, czy liczba jest podzielna przez 3 (reszta z dzielenia przez 3 wynosi 0).
-Jeśli liczba jest podzielna przez 3, wypisz ją.
+Zadanie 1: Liczby Parzyste i Nieparzyste
+Napisz program, który poprosi użytkownika o podanie liczby n, a następnie wyświetli wszystkie liczby od 1 do n. Dla każdej liczby, program powinien wskazać, czy jest parzysta, czy nieparzysta.
 
+Wskazówki:
 
+Użyj pętli for lub while do iteracji przez liczby od 1 do n.
+Użyj instrukcji if-else do sprawdzenia, czy liczba jest parzysta (liczba % 2 == 0), czy nieparzysta.
 """
-print("--------------------ZADANIE 2----------------")
-list =[] 
 
-for i in range(1,50): 
-  if i %3==0: 
+n = int(input("Wprowadź liczbę: "))
+list=[]
+
+i= 1
+while i <= n:  
+  print(i)
+  if i%2==0:
     list.append(i)
+  i += 1
 
 print(list)
 
-print("--------------------ZADANIE 3----------------")
+print("____________ Zadanie 2______________")
 
 """
-Ćwiczenie 3: Znajdowanie maksymalnej liczby
-Zadanie:
-Napisz program, który z listy liczb znajdzie i wypisze największą liczbę.
-Objaśnienie:
-Utwórz listę z kilkoma liczbami (np. numbers = [3, 7, 2, 10, 15, 1, 9]).
-Utwórz zmienną, która będzie przechowywać największą liczbę (np. max_number) i ustaw jej początkową wartość na pierwszy element listy.
-Użyj pętli for, aby przejść przez wszystkie liczby w liście.
-W każdej iteracji porównaj aktualną liczbę z wartością w zmiennej max_number.
-Jeśli aktualna liczba jest większa niż wartość w max_number, zaktualizuj max_number do tej liczby.
-Po zakończeniu pętli wypisz wartość zmiennej max_number.
-Powodzenia w rozwiązywaniu zadań! Jeśli będziesz potrzebować pomocy, chętnie pomogę.
+Zadanie 2: Suma Liczb
+Napisz program, który poprosi użytkownika o podanie liczby n, a następnie obliczy sumę wszystkich liczb od 1 do n.
+
+Wskazówki:
+
+Użyj pętli for lub while do iteracji przez liczby od 1 do n.
+Zsumuj te liczby wewnątrz pętli.
+
 """
 
+liczba = int(input("Wprowadź liczbę: "))
 
-numbers=[3, 7, 2, 10, 15, 1, 9]
-
-max_numer = numbers[0]
-
-for i in numbers:
-  if i > max_numer: 
-    max_numer = i 
-
-
-print(max_numer)
+Zbiór = []
+i = 0
+while i <= liczba:
+  Zbiór.append(i)
+  i +=1 
 
 
+suma = 0
 
-print("-----done----")
-print("-----done----")
+for el in Zbiór: 
+    suma += el 
 
+Zbiór.reverse()
+
+print(Zbiór)
+
+print(suma)
