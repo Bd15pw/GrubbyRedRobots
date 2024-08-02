64989917b687df86b5041a6efc8d9ec20fa68746
Bd15pw <136353082+Bd15pw@users.noreply.github.com>
2024-08-02 10:48:36 +0000

    cwi8czenie

---
+++ funkcje.py
@@ -0,0 +1,12 @@
+def kantor(*kwoty,kurs=4.20):
+  lista_kwot=[]
+  for kwota in kwoty: 
+    wymiana= round(kwota/ kurs, 2)
+    lista_kwot.append(wymiana)
+  return lista_kwot
+
+wymianka = kantor(40,10,50,10,20,22,1,21,23)
+plik = open("kantor.txt", "w")
+
+for kwota in wymianka:
+  plik.write(str(kwota) + "\n")
--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-def kantor(*kwoty,kurs=4.20):
-  lista_kwot=[]
-  for kwota in kwoty: 
-    wymiana= round(kwota/ kurs, 2)
-    lista_kwot.append(wymiana)
-  return lista_kwot
-  
-wymianka = kantor(40,10,50,10,20,22,1,21,23)
-plik = open("kantor.txt", "w")
-
-for kwota in wymianka:
-  plik.write(str(kwota) + "\n")
+def dodaj(*args):
+  suma = 0
+  for liczba in args:
+    suma += liczba
+  print(suma)
+dodaj(10,15,14)
