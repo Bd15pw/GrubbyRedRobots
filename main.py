4e88c64f2639bf89ba97651410141ea0d4a8e80a
Bd15pw <136353082+Bd15pw@users.noreply.github.com>
2024-08-05 12:15:58 +0000

    web scraping tekstu dynamicznego

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import time
 
 def get_driver():
   #Set options 
@@ -16,9 +17,18 @@ def get_driver():
   driver.get("http://automated.pythonanywhere.com")
   return driver
 
+def clean_text(text):
+  """Extract only the temperature from text"""
+  output = float(text.split(": ")[1])
+  return output
+  
+
+
+
 def main():
-  driver = get_driver()  
-  element = driver.find_element(By.XPATH, "/html/body/div[1]/div/h1[1]")  
-  return element.text
+  driver = get_driver()
+  time.sleep(2)
+  element = driver.find_element(By.XPATH, "/html/body/div[1]/div/h1[2]")  
+  return clean_text(element.text)
 
 print(main())
