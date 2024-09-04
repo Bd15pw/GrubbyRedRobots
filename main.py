08c9d046452643c2697ac9a087e28dbd5e4f92d6
Bd15pw <136353082+Bd15pw@users.noreply.github.com>
2024-09-04 15:44:38 +0000

    Weather Api

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import requests
 
-r = requests.get('https://rickandmortyapi.com/api/character')
+def getWeather(city,units='metric', api_key='13ab70d1ba6b1c1421505657c5d5d548'):
+  url = f'http://api.openweathermap.org/data/2.5/forecast?q={city}&appid={api_key}&units={units}'
+  r = requests.get(url)
+  content =r.json()
+  
+  with open('data.txt', 'a') as file:
+    for item in content['list']:
+      file.write(f"{item['dt_txt']}, {item['main']['temp']}\n")
 
-content = r.json()
-characters = content['results']
+  
+  
 
-
-
-for x in characters:
-  print(x['name'] +": "+ x['location']['name']['name'])
-  print(x['name'])
-  print(x['name'])
+print(getWeather(city="Wrocław"))
+  
