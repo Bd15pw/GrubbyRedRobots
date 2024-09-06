c455459ed56ec177ab599e95d002695e40ca934e
Bd15pw <136353082+Bd15pw@users.noreply.github.com>
2024-09-06 17:30:49 +0000

    Working with Pathlib

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
-import requests
+from pathlib import Path
 
-def getWeather(city,units='metric', api_key='13ab70d1ba6b1c1421505657c5d5d548'):
-  url = f'http://api.openweathermap.org/data/2.5/forecast?q={city}&appid={api_key}&units={units}'
-  r = requests.get(url)
-  content =r.json()
-  
-  with open('data.txt', 'a') as file:
-    for item in content['list']:
-      file.write(f"{item['dt_txt']}, {item['main']['temp']}\n")
+p1 = Path('files/abc.txt')
 
-  
-  
-
-print(getWeather(city="Warszawa"))
-  
+with open(p1, 'r') as file:
+  print(file.read())
